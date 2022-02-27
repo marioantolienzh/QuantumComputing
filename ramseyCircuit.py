9a1de3b15097f0bbfd8aacaf915ba4d09795fddf
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2022-02-27 12:26:13 -0500

    Update ramseyCircuit.py

--- ramseyCircuit.py
+++ ramseyCircuit.py
@@ -1,1 +1,3 @@
-//This code implements a Ramsey Quantum Circuit used to perform a variation in the phase of a single qubit state |s> = |0>
+#This code implements a Ramsey Quantum Circuit used to perform a variation in the phase of a single qubit state |s> = |0> using Qiskit
+
+
